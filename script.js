bae9996140c27b53b3acb03d2cdcc2dcd4773d7a
jonerdarron-glitch <jonerdarron@gmail.com>
2026-09-22 22:08:17 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -25,6 +25,12 @@ function notify(message) {
   }, 2500);
 }
 
+const menuToggle = document.getElementById("menuToggle");
+    const navMenu = document.getElementById("navMenu");
+
+    menuToggle.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+    });
 
 /* OPEN MODALS */
 
